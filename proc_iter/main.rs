35f8568255d5d41d348da4daf0450e1ba4ca3aaa
Milan <milanlandaverde@gmail.com>
2021-10-07 15:57:30 -0400

    table format of proc info with tgid

--- proc_iter/main.rs
+++ proc_iter/main.rs
@@ -2,32 +2,39 @@
 #![feature(allocator_api, global_asm)]
 
 use kernel::prelude::*;
-use kernel::task::ProcessIterator;
+use kernel::task::{ProcessIterator, Task};
 
-struct ProcessIteratorModule;
+struct ProcIterModule;
 
 module! {
-    type: ProcessIteratorModule,
-    name: b"process_iterator",
+    type: ProcIterModule,
+    name: b"proc_iter",
     author: b"milan@mdaverde.com",
     description: b"A kernel module",
     license: b"Dual MIT/GPL",
 }
 
+fn print_process(task: &Task) {
+    pr_info!("| {:>5} | {:>5} |", task.pid(), task.tgid());
+}
+
 fn show_processes() {
     let proc_iter = ProcessIterator::new();
-    for proc in proc_iter {
-        pr_info!("pid {:?}", proc.pid());
+    pr_info!("| {:>5} | {:>5} |", "PID", "TGID");
+    // logs first 25 tasks
+    for (i, proc) in (0..25).zip(proc_iter) {
+        print_process(&proc);
+        // TODO: consider cond_resched()
     }
 }
 
-impl KernelModule for ProcessIteratorModule {
+impl KernelModule for ProcIterModule {
     fn init() -> Result<Self> {
         show_processes();
-        Ok(ProcessIteratorModule)
+        Ok(ProcIterModule)
     }
 }
 
-impl Drop for ProcessIteratorModule {
+impl Drop for ProcIterModule {
     fn drop(&mut self) {}
 }
